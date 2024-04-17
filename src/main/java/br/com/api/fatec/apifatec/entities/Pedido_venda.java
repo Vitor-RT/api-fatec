aab939812706516262f017e2ca47dde770e0ee8d
Vitor Rodrigues Trindade <vitor.rtrindade@outlook.com>
2024-04-17 19:06:50 -0300

    Ajuste no código das entidades de pedido-venda

--- src/main/java/br/com/api/fatec/apifatec/entities/Pedido_venda.java
+++ src/main/java/br/com/api/fatec/apifatec/entities/Pedido_venda.java
@@ -15,15 +15,68 @@ public class Pedido_venda {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Date emissao;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
 
-    @Column(nullable = false)
-    private Double total;
+    @Column(name = "emissao", nullable = false)
+    private LocalDate emissao;
 
-    @Column(nullable = false)
-    private Integer cliente_id;
+    @Column(precision = 20, scale = 2, nullable = false)
+    private BigDecimal total;
 
-    @Column(nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING, nullable = false)
+    private PedidoVendaStatusEnum status;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pedidoVenda", orphanRemoval = true)
+    private List<PedidoVendaItem> items = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getEmissao() {
+        return emissao;
+    }
+
+    public void setEmissao(LocalDate emissao) {
+        this.emissao = emissao;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public PedidoVendaStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(PedidoVendaStatusEnum status) {
+        this.status = status;
+    }
+
+    public List<PedidoVendaItem> getItems() {
+        return items;
+    }
+
+    public void addItem(PedidoVendaItem item) {
+        this.items.add(item);
+        item.setPedidoVenda(this);
+    }
 }
