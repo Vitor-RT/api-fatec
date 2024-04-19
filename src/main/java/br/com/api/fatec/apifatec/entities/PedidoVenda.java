fd79aeb32e9ac89c2cb840ac923a6dee142a741a
Vitor Rodrigues Trindade <vitor.rtrindade@outlook.com>
2024-04-18 21:17:24 -0300

    Ajustes erros

--- src/main/java/br/com/api/fatec/apifatec/entities/PedidoVenda.java
+++ src/main/java/br/com/api/fatec/apifatec/entities/PedidoVenda.java
@@ -1,16 +1,27 @@
 package br.com.api.fatec.apifatec.entities;
 
+import java.math.BigDecimal;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.api.fatec.apifatec.shared.enums.PedidoVendaStatusEnum;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "pedido_venda")
-public class Pedido_venda {
+public class PedidoVenda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/br/com/api/fatec/apifatec/entities/PedidoVendaItem.java
+++ src/main/java/br/com/api/fatec/apifatec/entities/PedidoVendaItem.java
@@ -1,11 +1,16 @@
 package br.com.api.fatec.apifatec.entities;
 
-import java.sql.Date;
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
